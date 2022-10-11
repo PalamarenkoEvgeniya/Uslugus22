196bb589de05c9947ccfc837c4cdddedcc2bfa56
Eugenia Palamarenko <palamarenko.evgeniya@gmail.com>
2022-10-11 08:17:23 +0300

    add stars-rating

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import {avatarController} from "./modules/avatarController";
 import {getCategory} from "./modules/getCategory";
 import {renderList} from "./modules/renderList";
 import {searchControl} from "./modules/searchControl";
+import {ratingController} from "./modules/ratingController";
 
 
 const init = () => {
@@ -71,6 +72,7 @@ const init = () => {
   getCategory();
   renderList();
   searchControl();
+  ratingController();
 };
 
 init();
